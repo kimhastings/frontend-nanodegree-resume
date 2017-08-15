f44ffcca36e518b5c364d5b2c2cbe2340897c5c1
Kim Hastings <kimhastings@users.noreply.github.com>
2017-08-15 15:56:56 -0700

    Add contact info to footer

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,18 +19,18 @@ var bio = {
     ],
     "biopic": "images/owl.jpg",
     "display": function() {
-        $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-        $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+        $("#topContacts, #footerContacts").prepend(HTMLheaderRole.replace("%data%", bio.role));
+        $("#topContacts, #footerContacts").prepend(HTMLheaderName.replace("%data%", bio.name));
 
-        $("#header").append(HTMLemail.replace("%data%", bio.contacts.email));
-        $("#header").append(HTMLgithub.replace("%data%", bio.contacts.github));
-        $("#header").append(HTMLlocation.replace("%data%", bio.contacts.location));
+        $("#topContacts, #footerContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+        $("#topContacts, #footerContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+        $("#topContacts, #footerContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
 
-        $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
-        $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
-        $("#header").append(HTMLskillsStart);
+        $("#topContacts, #footerContacts").append(HTMLbioPic.replace("%data%", bio.biopic));
+        $("#topContacts, #footerContacts").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+        $("#topContacts, #footerContacts").append(HTMLskillsStart);
         bio.skills.forEach(function(skill) {
-            $("#header").append(HTMLskills.replace("%data%", skill));
+            $("#topContacts, #footerContacts").append(HTMLskills.replace("%data%", skill));
         });
     }
 };
